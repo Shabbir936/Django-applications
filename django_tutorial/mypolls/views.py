c21d6d844bc7ad90668efeabaa1e6b912a020291
Shabbir936 <S.hotel936@gmail.com>
2024-05-05 00:10:03 +0530

    Django tutorial complete - Day 2

--- django_tutorial/mypolls/views.py
+++ django_tutorial/mypolls/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+from django.utils import timezone
 from django.db.models.query import QuerySet
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
@@ -10,17 +11,32 @@ from django.views import generic
 
 # Create your views here.
 
+
 class IndexView(generic.ListView):
     template_name = "mypolls/index.html"
     context_object_name = "latest_question_list"
-    
+
     def get_queryset(self) -> QuerySet[Any]:
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published questions (not including those to be
+        published in the)
+
+        Returns:
+            QuerySet[Any]: List of 5 Questions in descending of published date
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "mypolls/detail.html"
     
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "mypolls/results.html"
